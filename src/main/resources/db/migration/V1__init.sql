5cbf2470bf4b08788bb3f1bda14322e1546dc3f0
VGolovkina <VGolovkina@cbgr.ru>
2022-02-08 17:17:09 +0300

    TB-1 Написан sql-скрипт создания структуры бд. Настроен flyway.

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,1 +1,19 @@
 drop table if exists users cascade;
+drop table if exists events cascade;
+
+CREATE TABLE users
+(
+    id         SERIAL               NOT NULL,
+    chat_id    INTEGER UNIQUE       NOT NULL,
+    name       VARCHAR              NOT NULL,
+    active     BOOLEAN              NOT NULL
+);
+
+CREATE TABLE events
+(
+    id         SERIAL           NOT NULL,
+    type       VARCHAR          NOT NULL,
+    name       VARCHAR          NOT NULL,
+    data       TIMESTAMP        NOT NULL,
+    sending     BOOLEAN         NOT NULL
+);
